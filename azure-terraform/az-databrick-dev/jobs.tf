7f3ab2f1e95b2d632bf34e199f52d8c1ef37aa4e
gayatrialamuri <newsun.a@gmail.com>
2026-05-23 15:51:21 -0400

    databricks spark job added

--- azure-terraform/az-databrick-dev/jobs.tf
+++ azure-terraform/az-databrick-dev/jobs.tf
@@ -5,7 +5,7 @@ resource "databricks_job" "run_notebook" {
     task_key = "sparkjob_task"
 
     notebook_task {
-      notebook_path = "/Repos/newsun_a@yahoo.com/Flintnas/notebooks/create_sparkjob.py"
+      notebook_path = "/Repos/newsun_a@yahoo.com/Flintnas/notebooks/test-sparkjob.py"
     }
 
     existing_cluster_id = databricks_cluster.single_node_dev.id
